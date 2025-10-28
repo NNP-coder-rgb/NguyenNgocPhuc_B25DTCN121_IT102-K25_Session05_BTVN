20c310ecf87f5656827d9d81289544a8fd386d18
NNP-coder-rgb <nguyenphuc230607@gmail.com>
2025-10-28 19:27:49 +0700

    Update EX9.c

--- SESSION_5/EX9.c
+++ SESSION_5/EX9.c
@@ -8,36 +8,41 @@ int main (){
 	float He_so_luong;
 	printf("He so luong cua nguoi do la: ");
 	scanf("%f",&He_so_luong);
+
+	int chucVu;
+	printf(" 1.Nhan vien\n 2.To truong\n 3.Quan ly\nNhap vao chuc vu :");
+	scanf("%d",&chucVu);
 	
 	int Hop_le = 1    ;
-	if(so_ngay_cong < 0){
-		Hop_le = 0;
-	}
-	if(He_so_luong < 0){
-		Hop_le = 0;
-	}
-	if(so_ngay_cong > 31){
+	if(so_ngay_cong < 0 || so_ngay_cong > 31 || He_so_luong < 0 || chucVu < 1 || chucVu > 3){
 		Hop_le = 0;
 		
 	}
-	float luong_nhan_vien,
-		  luong_to_truong,
-		  luong_quan_ly;
+	int phu_cap;
+	switch(chucVu){
+		case 1:
+		phu_cap = 500000;
+		break;
+		case 2:
+		phu_cap = 1000000;
+		break;
+		case 3:
+		phu_cap = 2000000;
+		break;
+	}
+	float luong;
 	
 	
 	if(so_ngay_cong > 26){
-		luong_nhan_vien = so_ngay_cong * 160000 * He_so_luong + 500000 + (so_ngay_cong - 26) * 200000;
-		luong_to_truong = so_ngay_cong * 160000 * He_so_luong + 1000000 + (so_ngay_cong - 26) * 200000;
-		luong_quan_ly = so_ngay_cong * 160000 * He_so_luong + 2000000 + (so_ngay_cong - 26) * 200000;
+		luong = so_ngay_cong * 160000 * He_so_luong + phu_cap + (so_ngay_cong - 26) * 200000;
+	
 	}else{
-		luong_nhan_vien = so_ngay_cong * 160000 * He_so_luong + 500000;
-		luong_to_truong = so_ngay_cong * 160000 * He_so_luong + 1000000;
-		luong_quan_ly = so_ngay_cong * 160000 * He_so_luong + 2000000;
+		luong = so_ngay_cong * 160000 * He_so_luong + phu_cap;
+		
 	}
 	if(Hop_le){
-		printf("Luong nhan vien nhan duoc trong thang nay la: %.f VND\n",luong_nhan_vien);
-		printf("Luong to truong nhan duoc trong thang nay la: %.f VND\n",luong_to_truong);
-		printf("Luong quan ly nhan duoc trong thang nay la: %.f VND\n",luong_quan_ly);
+		printf("Luong nhan vien nhan duoc trong thang nay la: %.f VND\n",luong);
+		
 	}else{
 		printf("Loi: Thong tin khong hop le, vui long nhap lai!");
 }
